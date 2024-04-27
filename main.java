36e4ff185abc2ea1b939a2284da1793f1e8fa8ca
LOLkol733 <149338633+LOLkol733@users.noreply.github.com>
2024-04-27 21:46:27 +1000

    Add files via upload

---
+++ main.java
@@ -0,0 +1,9 @@
+import shop.Shop;
+
+// Загрузка меню из метода mainMenu в классе Shop
+public class main {
+    public static void main(String[] args) {
+        Shop shop = new Shop();
+        shop.mainMenu();
+    }
+}
